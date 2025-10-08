ef55e8f4c982eeb1c75d83d1dc408debc1640d4d
Abhishek Thombre <abhishekthombre25@gmail.com>
2025-10-08 08:29:12 +0530

    Initial commit

---
+++ Wordle.java
@@ -0,0 +1,12 @@
+
+class Wordle{
+
+    public static void main(String[] args) {
+        String[] words={"Can","May","Say","Who","Why","Pen","Sim"};
+        Boolean[] flag={false,false,false};
+        int guessCount=0;
+        for(String word: words){
+            System.out.println(word);
+        }
+    }
+}
