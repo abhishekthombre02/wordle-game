fbd1c2a73b44e024ebddeee875e223456568e8b8
Abhishek Thombre <abhishekthombre25@gmail.com>
2025-10-08 08:58:57 +0530

    inputs added

--- Wordle.java
+++ Wordle.java
@@ -1,12 +1,26 @@
-
+import java.util.Scanner;
 class Wordle{
 
     public static void main(String[] args) {
+
+        Scanner read=new Scanner(System.in);
+
         String[] words={"Can","May","Say","Who","Why","Pen","Sim"};
         Boolean[] flag={false,false,false};
-        int guessCount=0;
+        String guessWord="XXX";
+
+        int guessCount=6;
+        for(int i=0;i<6;i++){
+            System.out.println(guessWord);
+            System.out.println("Enter index: ");
+            int index=read.nextInt();
+
+        }
+        
+        System.out.println();
         for(String word: words){
             System.out.println(word);
         }
     }
+
 }
